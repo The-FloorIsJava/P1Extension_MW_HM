f62faeda61b891e210285b3d744e87006b7508ed
mwigand086 <mwigand086@gmail.com>
2022-12-07 13:54:41 -0800

    Added starter JS script for login page, updated login.html title

---
+++ FrontEnd/login/login.js
@@ -0,0 +1,7 @@
+let loginBlank = document.getElementById("login-form").innerHTML
+const url = "http://localhost:8080/"
+
+function userLogin(form){
+    let username = form.username.value;
+    let password = form.password.value;
+}
