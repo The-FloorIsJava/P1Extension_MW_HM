49393607162e9c1c1204593cd6a85f7c5e6defe2
mwigand086 <mwigand086@gmail.com>
2022-12-07 14:06:21 -0800

    Added skeletonized function to be built further shortly

--- FrontEnd/login/login.js
+++ FrontEnd/login/login.js
@@ -4,4 +4,8 @@ const url = "http://localhost:8080/"
 function userLogin(form){
     let username = form.username.value;
     let password = form.password.value;
+
+    fetch()
+    .then()
+    .catch()
 }
