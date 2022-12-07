9b7d9e969eda6c3e20aae8e54a6a227beb83bc1b
mwigand086 <mwigand086@gmail.com>
2022-12-07 14:55:35 -0800

    More skeletonization, added logout function stubs and stubbed then/catch methods

--- FrontEnd/login/login.js
+++ FrontEnd/login/login.js
@@ -5,7 +5,25 @@ function userLogin(form){
     let username = form.username.value;
     let password = form.password.value;
 
-    fetch()
-    .then()
-    .catch()
+    fetch(`${url}login`, {
+        method: 'POST',
+        body: JSON.stringify({
+            userName: username,
+            password: password
+        })
+    })
+    .then(response =>{
+        /*response will go here*/
+    })
+    .catch(error => {
+        /*error handling will go here*/
+    })
+}
+
+function logoutButton(){
+
+}
+
+function logout(){
+
 }
